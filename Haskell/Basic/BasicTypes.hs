cc798c4206a256c529c3985d038578ac08f5b4e9
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2017-10-14 13:02:39 +0200

    Update Haskell sheet.

---
+++ Haskell/Basic/BasicTypes.hs
@@ -0,0 +1,4 @@
+module BasicTypes where
+
+-- This file contains exercises about basic types (Int, Double, Float, Bool, Char, String),
+-- lists, tuples, control elements like IF...THEN, CASE and |.
