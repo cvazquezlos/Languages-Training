9764d875bd8a01baced8d5db6a7c3be0ab19d2bd
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2017-10-18 10:23:19 +0200

    Added two last algorithms.

--- Haskell/Type declarations/Types2.hs
+++ Haskell/Type declarations/Types2.hs
@@ -71,7 +71,42 @@ toString (x:xs) = if (x < 10) then "0" ++ show x ++ "/" ++ toString xs
 								 		
 -- G. Compare two dates.
 instance Eq Dat where
-Date d1 m1 y1 == Date d2 m2 y2 = (d1 == d2) && (m1 == m2) && (y1 == y2)
+    Date d1 m1 y1 == Date d2 m2 y2 = (d1 == d2) && (m1 == m2) && (y1 == y2)
 
 ejG:: Dat -> Dat -> Bool
 ejG x y = x == y
+
+-- H. Implement a instance to make quicksort works.
+instance Ord Dat where
+	Date d1 m1 y1 <= Date d2 m2 y2 = (y1 <= y2) || ((y1 == y2) && (m1 <= m2))
+	Date d1 m1 y1 >= Date d2 m2 y2 = (y1 >= y2) || ((y1 == y2) && (m1 >= m2))
+	Date d1 m1 y1 < Date d2 m2 y2 = (y1 < y2) || ((y1 == y2) && (m1 < m2))
+	Date d1 m1 y1 > Date d2 m2 y2 = (y1 > y2) || ((y1 == y2) && (m1 > m2))
+
+-- I. Some classes declarations.
+class Collection c where
+  	isEmpty :: c a -> Bool
+  	insert :: a -> c a -> c a 
+  	first :: c a -> a
+  	delete :: c a -> c a 		
+    	size :: c a -> Int	
+
+
+data Pila a = Pil [a] deriving Show
+
+instance Collection Pila where
+    isEmpty (Pil p) = length p == 0 
+    insert e (Pil p) = Pil (p ++ [e])
+    first (Pil p) = last p
+    delete (Pil p) = Pil (init p)
+    size (Pil l) = length l
+
+								
+data Cola a = Col [a] deriving Show
+
+instance Collection Cola where
+    isEmpty (Col c) = length c == 0
+    insert e (Col c) = Col (c ++ [e])
+    first (Col c) = head c
+    delete (Col (c:cs)) = Col cs
+    size (Col c) = length c
