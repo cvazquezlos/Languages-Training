ad9160391b4788d0262748ec299690419dc150cc
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2017-11-06 16:51:00 +0100

    Solved little errors.

--- Haskell/Recursion/Recursion.hs
+++ Haskell/Recursion/Recursion.hs
@@ -5,23 +5,27 @@ module Recursion where
 -- Tail recursion = Recursividad final.
 
 -- A. Given a list, delete from it those numbers that are multiple of one given.
-ejA:: [Int] -> Int -> [Int] -- List comprehension.
-ejA x n = [y | y <- x, not (y `mod` 2 == 0)]
+ej2A:: [Int]->Int->[Int]
+ej2A x n = [y|y<-x, y`mod`n/=0]
 
-ejA':: [Int] -> Int -> [Int] -- Primitive recursion.
-ejA' [] _ = []
-ejA' (x:xs) n = if (x `mod` n == 0) then ejA' xs n else x:(ejA' xs n)
+ej2A':: [Int]->Int->[Int] -- Recursión no final
+ej2A' [] _ = []
+ej2A' (x:xs) n = if (x`mod`n/=0) then f++[x]
+				 else f
+		    where
+		       f = ej2A' xs n
+				 		
+ej2A'':: [Int]->Int->[Int] -- Recursividad final
+ej2A'' x n = ej2A''Aux x n []
 
-ejA'':: [Int] -> Int -> [Int] -- Tail recursion.
-ejA'' x n = ejA''Aux x n []
-
-ejA''Aux:: [Int] -> Int -> [Int] -> [Int]
-ejA''Aux [] _ acum = acum
-ejA''Aux (x:xs) n acum = if (x `mod` n == 0) then ejA''Aux xs n acum else ejA''Aux xs n (x:acum)
+ej2A''Aux:: [Int]->Int->[Int]->[Int]
+ej2A''Aux [] _ acum = acum
+ej2A''Aux (x:xs) n acum = if (x`mod`n/=0) then ej2A''Aux xs n (acum++[x])
+										  else ej2A''Aux xs n acum
 
 -- B. Write doble function just with lambda expressions.
 ejB:: Int -> Int
-ejB = (\ x -> x * x)
+ejB = (\ x -> x + x)
 
 -- C. Given a list, return the multiplication of its numbers.
 ejC:: [Int] -> Int -- Primitive recursion.
@@ -60,20 +64,12 @@ contains ((x, _):xs) n = if (x==n) then True
 				   else contains xs n
 
 -- F. Given a list, return the number of 0 inside of it.
-ejF:: [Int] -> Int -- Primitive recursion
-ejF [] = 0
-ejF (x:xs) = if x == 0 then 1 + ejF xs else 0 + ejF xs
-
-ejF':: [Int] -> Int -- Tail recursion
-ejF' x = ejF'Aux x 0
-
-ejF'Aux:: [Int] -> Int -> Int
-ejF'Aux [] acum = acum
-ejF'Aux (x:xs) acum = if x == 0 then acum + 1 + (ejF'Aux xs acum) 
-				  else acum + (ejF'Aux xs acum)
-                                  
-ejF'':: [Int] -> Int -- List comprehension.
-ejF'' x = length [y | y <- x, y == 0]
+ej2F:: [Int]->Int
+ej2F [] = 0
+ej2F [0,_] = 1
+ej2F (x:y:xs) = if ((x==0)&&(y/=0)) then (1+f) else (0+f) 
+		   where
+		      f = ej2F (y:xs)
 
 -- G. Given a list, return two lists: the first with the repeated numbers, the last one with the others.
 ejG:: [Int] -> ([Int], [Int])
